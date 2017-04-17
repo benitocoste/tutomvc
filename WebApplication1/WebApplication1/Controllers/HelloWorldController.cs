329d08d082ede3d0b1d6f8f97c8b903763728179
Benoit COSTE <coste.benoit@gmail.com>
2017-04-17 17:44:46 +0200

    modif de la route config et création d'un controller

---
+++ WebApplication1/WebApplication1/Controllers/HelloWorldController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: HelloWorld
+        public string Index()
+        {
+            return "Bienvenue sur l'index";
+        }
+        //GET : /Helloworld/
+        public string HelloWorld()
+        {
+            return "Bonjour le monde";
+        }
+    }
+}
